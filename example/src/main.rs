bef52c2330e19e79f572d31e0d7577d0c10e30e1
ufnkam <ufnkam@outlook.com>
2026-06-29 00:21:54 +0200

    Cleaning + icons support

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,10 +1,9 @@
-use clap::{Parser};
+use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[command(name = "example")]
 #[command(about = "Tiny example CLI for cargo-crapapp")]
 struct Cli {}
-
 
 fn main() {
     let _cli = Cli::parse();
