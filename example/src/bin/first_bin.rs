8c20a5e12809aba5fd51df8ef1cfec2d4000280f
ufnkam <ufnkam@outlook.com>
2026-07-19 19:45:30 +0200

    feat: extend example

--- example/src/bin/first_bin.rs
+++ example/src/bin/first_bin.rs
@@ -1,13 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "first_bin", version, about = "Example packaged CLI utility")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Print a greeting a configurable number of times.
+    Greet {
+        #[arg(long, default_value = "developer")]
+        name: String,
+        #[arg(long, default_value_t = 1)]
+        times: u8,
+    },
+    /// Print compile-time feature information.
+    Features,
+}
+
 fn main() {
-    println!("Hello and burn this world!");
+    match Cli::parse().command {
+        Command::Greet { name, times } => {
+            for _ in 0..times {
+                println!("Hello, {name}. first_bin is reachable from the package.");
+            }
+        }
+        Command::Features => {
+            #[cfg(feature = "some_feature")]
+            println!("some_feature is enabled");
 
-    #[cfg(feature = "some_feature")]
-    {
-        println!("I'm using some_feature!")
-    }
-
-    #[cfg(not(feature = "some_feature"))]
-    {
-        println!("I'm not using some_feature!")
+            #[cfg(not(feature = "some_feature"))]
+            println!("some_feature is disabled");
+        }
     }
 }
